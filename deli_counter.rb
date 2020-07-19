5ab32bc1656a7474666f3203f2dca0599eb3045b
Christopher Holloway <mrcholloway@yahoo.com>
2020-07-19 23:49:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,10 @@
 function takeANumber(katzDeliLine,name){
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
-}
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0) return `Currently serving ${katzDeliLine.shift()}.`;
   else return "There is nobody waiting to be served!";
-}
 
 function currentLine(katzDeliLine){
   var str = `The line is currently: `;
@@ -18,5 +16,3 @@ function currentLine(katzDeliLine){
       }
     str += newLine.join(', ')
     return str;
-  }
-}
